ab600efd2013647ac03d5b8325f6c9795ad6357a
Tapia-GJ <luivstapia285@gmail.com>
2024-07-15 12:18:13 -0500

    inventoryCrud

--- src/inventarios/inventarios.controller.ts
+++ src/inventarios/inventarios.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, ParseIntPipe, Put, Delete } from '@nestjs/common';
 import { InventariosService } from './inventarios.service';
 import { Inventario } from './entities/inventario.entity';
 import { CreateInventoryDto } from './dto/create-inventario.dto';
+import { UpdateEmpleadoDto } from 'src/empleados/dto/update-empleado.dto';
+import { UpdateInventoryDto } from './dto/update-inventario.dto';
 
 @Controller('inventarios')
 export class InventariosController {
@@ -11,9 +13,20 @@ export class InventariosController {
   async findAll(): Promise<Inventario[]> {
     return await this.inventarioService.findAll();
   }
+  @Get(':id')
+  async getEmpleado(@Param('id', ParseIntPipe) id: number): Promise<Inventario> {
+    return await this.inventarioService.getInventory(id);
+  }
   @Post()
   async createInventory(@Body() newInventory: CreateInventoryDto): Promise<Inventario> {
     return await this.inventarioService.createInventario(newInventory);
   }
-  
+  @Put(':id')
+  async updateEmpleado(@Param('id', ParseIntPipe) id: number, @Body() inventory: UpdateInventoryDto){
+    return await this.inventarioService.updateInventory(id, inventory);
+  }
+  @Delete(':id')
+  async deleteEmpleado(@Param('id', ParseIntPipe) id: number){
+    return await this.inventarioService.deleteInventory(id);
+  }
 }
